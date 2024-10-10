3dac53f19ad871f10e206a047a3c4f93f5361bd1
Matías Poirier <97362850+matipoirierg@users.noreply.github.com>
2024-10-10 20:59:01 -0300

    Update votelinks.js

--- app/votelinks.js
+++ app/votelinks.js
@@ -1,9 +1,8 @@
 const voteLinks = [
-    { id: 1, url: "https://www.planetminecraft.com/", site: "www.planetminecraft.com" },
-    { id: 2, url: "https://minecraft-server-list.com/", site: "minecraft-server-list.com" },
-    { id: 3, url: "https://minecraft.buzz/", site: "minecraft.buzz" },
-    { id: 4, url: "https://minecraft-mp.com/", site: "minecraft-mp.com" },
+    { id: 1, url: "https://minecraft-mp.com/server/280159/vote/", site: "minecraft-mp.com" },
+    { id: 2, url: "https://servidoresdeminecraft.es/server/vote/bXX1Fnnx/play.eventyr.lat", site: "servidoresdeminecraft.es" },
+    { id: 3, url: "https://topminecraftservers.org/vote/37757", site: "topminecraftservers.org" },
+    { id: 4, url: "https://topg.org/es/servidores-de-minecraft/server-624563", site: "topg.org" },
     { id: 5, url: "https://topminecraftservers.org/", site: "topminecraftservers.org" },
-    { id: 6, url: "https://minecraftservers.org/", site: "minecraftservers.org" },
     ]
 export default voteLinks
